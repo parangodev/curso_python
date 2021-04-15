c28a06bca7be254eb8f1edd3661a1d70a69685e9
parango26 <pabloarangobte@gmail.com>
2021-04-14 20:18:50 -0500

    no message

---
+++ debugging.py
@@ -0,0 +1,19 @@
+def divisors(num):
+  divisors = []
+  for i in range(1, num + 1):
+    if num % i == 0:
+      divisors.append(i)
+  return divisors
+
+def run():
+  try:
+    assert 1 == 2, "mi error"
+    num = int(input("Ingresa un numero: "))
+    print(divisors(num))
+    print("Termino mi programa")
+  except ValueError:
+    print("Debes ingresar un numero")
+
+
+if __name__ == '__main__':
+  run()
